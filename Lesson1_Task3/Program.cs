f87c1d2ccb77d777cedb79c10d90f435168a6612
X_Finder <pollux094@gmail.com>
2015-07-01 13:09:09 +0200

    Добавление

---
+++ Lesson1_Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //объявление и инициализация массива
+            Purchase[] purchases = new Purchase[] 
+            {
+	            new Purchase("cup",38000,3,Days.Mon),
+                new Purchase("plate",50000,1,Days.Sun),
+                new Purchase("spoon",21000,4,Days.Wed),
+                new Purchase("fork",12000,4,Days.Fri),
+                new Purchase("knife",35000,2,Days.Sat),
+            };
+            double sum = 0; // переменная для нахождения суммы покупок
+            for (int i = 0; i < purchases.Length; i++)
+            {
+                if (purchases[i] != null)
+                {
+                    Console.WriteLine(purchases[i]);
+                    sum = sum + purchases[i].GetCost();
+                }
+            }
+            Array.Sort(purchases);
+            Console.WriteLine("\nСредняя стоимость всех покупок "+(sum/purchases.Length));
+            Console.WriteLine("День самой дорогой покупки " + purchases[0].Days+"\n");
+            for (int i = 0; i < purchases.Length; i++)
+            {
+                if (purchases[i] != null)
+                    Console.WriteLine(purchases[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson1_Task3/Purchase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_Task3
+{
+    enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+    class Purchase : IComparable<Purchase>
+    {
+        string name;
+        int price;
+        int number;
+        Days days;
+
+        public Purchase()
+        { }
+        public Purchase( string name, int price, int number, Days days)
+        {
+            this.name=name;
+            this.price=price;
+            this.number=number;
+            this.days = days;
+        }
+        /// <summary>
+        /// Стоимость покупки
+        /// </summary>
+        /// <returns></returns>
+        public int GetCost()
+        {
+            return (price * number);
+        }
+        /// <summary>
+        /// Метод сравнения по стоимости покупки
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public int CompareTo(Purchase p)
+        {
+            if (this.GetCost() > p.GetCost())
+                return -1;
+            else
+                if (this.GetCost() < p.GetCost())
+                    return 1;
+                else
+                    return 0;
+        }
+
+        public override string ToString()
+        {
+            return name+";"+price+";"+number+";"+days+";"+GetCost();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        public int Number
+        {
+            get { return number; }
+            set { this.number = value; }
+        }
+
+        public int Price
+        {
+            get { return price; }
+            set { this.price = value; }
+        }
+
+        public Days Days
+        {
+            get { return days; }
+            set { this.days = value; }
+        }
+    }
+}
